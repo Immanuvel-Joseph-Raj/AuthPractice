b7d6da9b7b6be56980fc7de593faaa70c7a75cd5
joseph <balashankar+1-at-132380471363>
2021-09-14 10:23:47 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/fake-backend';
+
+import { AppComponent } from './app.component';
+import { appRoutingModule } from '../app/app-routing.module';
+
+import { ErrorInterceptor } from '../app/_helpers/error.interceptor';
+import { JwtInterceptor } from '../app/_helpers/jwt.interceptor';
+import { HomeComponent } from '../app/home/home.component';
+import { LoginComponent } from '../app/login/login.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    appRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
